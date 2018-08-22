ccad8ee682602bd8d3d3b2b5824f9e132bc51c6e
JacobLinCool <jacoblin2011@gmail.com>
2018-08-22 22:07:39 +0800

    Add files via upload

---
+++ js/urldata.js
@@ -0,0 +1,11 @@
+function urldata(key) {
+    var u = location.href;
+    var n = key.length;
+    var i = u.search(key)+n+1;
+    var a = "";
+    while(u.charAt(i)!=""&&u.charAt(i)!="&") {
+        a += u.charAt(i);
+        ++i;
+    }
+    return a;
+}

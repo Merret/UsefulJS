0d0c709170f1de339e57170edac59a6e9c0e9721
JacobLinCool <jacoblin2011@gmail.com>
2018-08-22 21:52:12 +0800

    Update align.js

--- js/align.js
+++ js/align.js
@@ -1,5 +1,6 @@
 function align(id, mode) {
 	var elm = document.getElementById(id);
+	ele.style.position = "relative";
 	if (mode = "h") {
 		elm.style.left = (window.innerWidth - elm.width) / 2 + "px";
 	}
@@ -14,16 +15,19 @@ function align(id, mode) {
 
 function alignH(id) {
 	var elm = document.getElementById(id);
+	ele.style.position = "relative";
 	elm.style.left = (window.innerWidth - elm.width) / 2 + "px";
 }
 
 function alignV(id) {
 	var elm = document.getElementById(id);
+	ele.style.position = "relative";
 	elm.style.top = (window.innerHeight - elm.height) / 2 + "px";
 }
 
 function alignA(id) {
 	var elm = document.getElementById(id);
+	ele.style.position = "relative";
 	elm.style.left = (window.innerWidth - elm.width) / 2 + "px";
 	elm.style.top = (window.innerHeight - elm.height) / 2 + "px";
 }

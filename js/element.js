727c6297f9bd46db3132640ba18f2be38174eafe
JacobLinCool <jacoblin2011@gmail.com>
2018-10-11 19:28:58 +0800

    Update element.js

--- js/element.js
+++ js/element.js
@@ -2,8 +2,13 @@ var elm = function () {
 	var id = function(x) {
 		return document.getElementById(x);
 	}
+	var name = function(x) {
+		return document.getElementsByName(x);
+	}
 	return {
         ID: id,
-        id: id
+        id: id,
+        NAME: name,
+        name: name
 	}
 };

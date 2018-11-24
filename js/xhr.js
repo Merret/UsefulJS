682474da874d5876cad56c6911470ffb126ad272
JacobLinCool <jacoblin2011@gmail.com>
2018-11-24 21:56:12 +0800

    Update xhr.js

--- js/xhr.js
+++ js/xhr.js
@@ -1,65 +1,69 @@
-// JavaScript Document
 var xhr = (function () {
-	var GET = function (trg, sync = false) {
-		var anita;
-		var ann = new XMLHttpRequest();
-		ann.open('GET', trg, sync);
-		ann.onload = function () {
-			if (this.status === 200) {
-				console.log("xhr.GET responded: " + this.responseText);
-				anita = this.responseText;
-			} else {
-				console.log("xhr.GET Error. statue: " + this.status);
-				anita = "xhr.GET Error. statue: " + this.status;
-			}
-		};
-		ann.send();
-		return anita;
-	}
-	var POST = function (trg, data, ContentType = "application/json", sync = false) {
-		var anita;
-		var ann = new XMLHttpRequest();
-		ann.open('POST', trg, sync);
-		ann.setRequestHeader("Content-Type", ContentType);
-		ann.onload = function () {
-			if (this.status >= 200 && this.status < 300) {
-				console.log("xhr.POST responded: " + this.responseText);
-				anita = this.responseText;
-			} else {
-				console.log("xhr.POST Error. statue: " + this.status);
-				anita = "xhr.POST Error. statue: " + this.status;
-			}
-		};
-		ann.send(data);
-		return anita;
-	}
-	var PUT = function (trg, data, ContentType = "application/json", sync = false) {
-		var anita;
-		var ann = new XMLHttpRequest();
-		ann.open('PUT', trg, sync);
-		ann.setRequestHeader("Content-Type", ContentType);
-		ann.onload = function () {
-			if (this.status >= 200 && this.status < 300) {
-				console.log("xhr.PUT responded: " + this.responseText);
-				anita = this.responseText;
-			} else {
-				console.log("xhr.PUT Error. statue: " + this.status);
-				anita = "xhr.PUT Error. statue: " + this.status;
-			}
-		};
-		ann.send(data);
-		return anita;
-	}
-	
-	var help = "xhr.GET: xhr.GET(url)\nxhr.POST: xhr.POST(url, data, [ContentType='application/json'], [sync=false])\nxhr.PUT: xhr.PUT(url, data, [ContentType='application/json'], [sync=false])\nXHR.js is made by JacobLinCool\nGitHub: https://github.com/jacoblincool";
-
-	return {
-		GET: GET,
-		get: GET,
-		POST: POST,
-		post: POST,
-		PUT: PUT,
-		put: PUT,
-		help: help
-	}
+    var GET = function (trg, sync = false) {
+        var anita;
+        var ann = new XMLHttpRequest();
+        ann.open('GET', trg, sync);
+        ann.onload = function () {
+            if (this.status === 200) {
+                console.log("xhr.GET responded: " + this.responseText);
+                anita = this.responseText;
+            } else {
+                console.log("xhr.GET Error. statue: " + this.status);
+                anita = "xhr.GET Error. statue: " + this.status;
+            }
+        };
+        ann.send();
+        return anita;
+    };
+    var POST = function (trg, data, ContentType = "application/json", sync = false) {
+        var anita;
+        var ann = new XMLHttpRequest();
+        ann.open('POST', trg, sync);
+        ann.setRequestHeader("Content-Type", ContentType);
+        ann.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                console.log("xhr.POST responded: " + this.responseText);
+                anita = this.responseText;
+            } else {
+                console.log("xhr.POST Error. statue: " + this.status);
+                anita = "xhr.POST Error. statue: " + this.status;
+            }
+        };
+        ann.send(data);
+        return anita;
+    };
+    var PUT = function (trg, data, ContentType = "application/json", sync = false) {
+        var anita;
+        var ann = new XMLHttpRequest();
+        ann.open('PUT', trg, sync);
+        ann.setRequestHeader("Content-Type", ContentType);
+        ann.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                console.log("xhr.PUT responded: " + this.responseText);
+                anita = this.responseText;
+            } else {
+                console.log("xhr.PUT Error. statue: " + this.status);
+                anita = "xhr.PUT Error. statue: " + this.status;
+            }
+        };
+        ann.send(data);
+        return anita;
+    };
+    var JSONG = function (trg, sync = false) {
+        return JSON.parse(GET(trg, sync));
+    };
+    
+    var help = "xhr.GET: xhr.GET(url)\nxhr.POST: xhr.POST(url, data, [ContentType='application/json'], [sync=false])\nxhr.PUT: xhr.PUT(url, data, [ContentType='application/json'], [sync=false])\nXHR.js is made by JacobLinCool\nGitHub: https://github.com/jacoblincool";
+    
+    return {
+        GET: GET,
+        get: GET,
+        POST: POST,
+        post: POST,
+        PUT: PUT,
+        put: PUT,
+        JSON: JSONG,
+        json: JSONG,
+        help: help
+    }
 })()

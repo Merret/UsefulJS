879fdaea1bcf11121030bd3fdd0e4f34ab9933cf
JacobLinCool <jacoblin2011@gmail.com>
2018-11-28 22:47:42 +0800

    Update userInfo.js

--- js/userInfo.js
+++ js/userInfo.js
@@ -1,7 +1,7 @@
 var userInfo;
 if(typeof xhr === 'undefined') {
     var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/Merret/UsefulJS@0.5/js/xhr.min.js';
+    script.src = 'https://cdn.jsdelivr.net/gh/Merret/UsefulJS@0.5.2/js/xhr.min.js';
     script.onload = function() {getUserInfo();};
     document.head.appendChild(script);
 }
@@ -16,7 +16,7 @@ console.log("Getting userInfo: Prepared 2/2");
 function getUserInfo() {
 var keyList = ["53328f3f91ecf752a50eacd2853473766234634e0d29ce157dd416df", "8355101613c9848d941992940ae62443599538f0baff6361ec263667", "fb271985d70493b7aac593d416e883624a5e8eef68c72bd3b92d0f22"];
 var keyListIndex = Math.floor(Math.random()*3);
-userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex], false));
+userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex], false, false));
 userInfo.dnsServer = JSON.parse(xhr.get("https://"+DNSkey(32, '0123456789abcdefghijklmnopqrstuvwxyz')+".edns.ip-api.com/json")).dns;
 console.log("Getting userInfo: Analysing...");
 }

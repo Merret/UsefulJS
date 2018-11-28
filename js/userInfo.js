5eb4baed54ddf3b6ed0f77eace1fe328c2f164a8
JacobLinCool <jacoblin2011@gmail.com>
2018-11-28 23:39:24 +0800

    Update userInfo.js

--- js/userInfo.js
+++ js/userInfo.js
@@ -13,7 +13,7 @@ function getUserInfo() {
 var keyList = ["53328f3f91ecf752a50eacd2853473766234634e0d29ce157dd416df", "8355101613c9848d941992940ae62443599538f0baff6361ec263667", "fb271985d70493b7aac593d416e883624a5e8eef68c72bd3b92d0f22"];
 var keyListIndex = Math.floor(Math.random()*3);
 userInfo = JSON.parse(xhr.get("https://api.ipdata.co/?api-key="+keyList[keyListIndex], false, false));
-userInfo.dnsServer = JSON.parse(xhr.get("https://"+DNSkey(32, '0123456789abcdefghijklmnopqrstuvwxyz')+".edns.ip-api.com/json")).dns;
+userInfo.dnsServer = JSON.parse(xhr.get("https://"+DNSkey(32, '0123456789abcdefghijklmnopqrstuvwxyz')+".edns.ip-api.com/json", false, false)).dns;
 console.log("Getting userInfo: Analysing...");
 if(typeof UAParser === 'undefined') {
     var script = document.createElement('script');

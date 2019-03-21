a4559e7647a978ff3a88f69d8a4976b1f747c806
JacobLinCool <jacoblin2011@gmail.com>
2019-03-21 22:48:59 +0800

    Update element.js

--- js/element.js
+++ js/element.js
@@ -1,13 +1,13 @@
 var elm = (function () {
 	var id = function(x) {
 		return document.getElementById(x);
-	}
+	};
 	var name = function(x) {
 		return document.getElementsByName(x);
-	}
+	};
 	var create = function(x) {
 		return document.createElement(x);
-	}
+	};
 	return {
         ID: id,
         id: id,
